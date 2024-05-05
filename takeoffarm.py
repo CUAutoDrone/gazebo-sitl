9a6191165f295c5f6c423faf78fde44377a27dea
Byounghyun123 <kevinlee.number10@gmail.com>
2024-05-05 13:49:04 -0400

    added wait function

--- takeoffarm.py
+++ takeoffarm.py
@@ -1,25 +1,32 @@
 from pymavlink import mavutil
+import time
 the_connection = mavutil.mavlink_connection('udp:127.0.0.1:14550')
 the_connection.wait_heartbeat()
 
 #SET_MODE
 the_connection.set_mode('GUIDED')
 
-
 #ARMING
 the_connection.arducopter_arm()
-
 
 #TAKEOFF
 the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 10)
 msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
 print(msg)
 
-the_connection.wait_heartbeat()
+altitude = 0
+target_altitude = 10  # Target takeoff altitude
 
-
-
-
+# Wait for the drone to reach the target altitude
+while altitude < target_altitude:
+    msg = the_connection.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
+    if msg:
+        altitude = msg.alt / 1000.0  # Convert from millimeters to meters
+        print(f"Current altitude: {altitude} meters")
+        if altitude >= target_altitude:
+            print("Target altitude reached.")
+            break
+    time.sleep(1)  # Check every second
 
 #MOVEMENT
 the_connection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message(10,
